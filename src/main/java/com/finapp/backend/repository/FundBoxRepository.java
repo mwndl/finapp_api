61507609d33b43e3dbc7e74f9febf3112483cec9
“Marcos” <marcos.wiendl@gmail.com>
2025-04-15 15:15:54 -0300

    feat(fundbox): adds search by fundboxId and ownerId

--- src/main/java/com/finapp/backend/repository/FundBoxRepository.java
+++ src/main/java/com/finapp/backend/repository/FundBoxRepository.java
@@ -1,20 +1,14 @@
 package com.finapp.backend.repository;
 
-import com.finapp.backend.model.Deposit;
 import com.finapp.backend.model.FundBox;
-import com.finapp.backend.model.enums.TransactionType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.math.BigDecimal;
-import java.util.List;
+import java.util.Optional;
 
 public interface FundBoxRepository extends JpaRepository<FundBox, Long> {
-    List<FundBox> findByOwnerId(Long ownerId);
     boolean existsByOwnerIdAndName(Long ownerId, String name);
     Page<FundBox> findByOwnerId(Long ownerId, Pageable pageable);
-
+    Optional<FundBox> findByIdAndOwnerId(Long id, Long ownerId);
 }
