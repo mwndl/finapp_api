caf1e649282d401fd99de5c133c51ce768e34cb4
“Marcos” <marcos.wiendl@gmail.com>
2025-04-16 00:06:11 -0300

    feat(exception): adds 'invalid token' exception

--- src/main/java/com/finapp/backend/exception/ApiErrorCode.java
+++ src/main/java/com/finapp/backend/exception/ApiErrorCode.java
@@ -22,6 +22,7 @@ public enum ApiErrorCode {
 
     // 401 - Unauthorized
     AUTH_EMAIL_NOT_FOUND(401, "AUTH_EMAIL_NOT_FOUND", "Email not found", "The email provided was not found in the database."),
+    AUTH_INVALID_TOKEN(401, "AUTH_INVALID_TOKEN", "Invalid token" , "The token provided is invalid" ),
     EXPIRED_SESSION(401, "EXPIRED_SESSION", "Session expired", "Your session has expired. Please log in again to continue."),
     INVALID_CREDENTIALS(401, "INVALID_CREDENTIALS", "Invalid credentials", "Email or password is incorrect."),
     ACCOUNT_DEACTIVATED(401, "ACCOUNT_DEACTIVATED", "Account is deactivated", "Your account is in the process of being deleted. Please log in again to reactivate your account."),
