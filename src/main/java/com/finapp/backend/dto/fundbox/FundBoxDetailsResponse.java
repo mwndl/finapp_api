d58239bd760bea2a62a63d9b26e74fd52b4fbde6
“Marcos” <marcos.wiendl@gmail.com>
2025-04-19 23:33:25 -0300

    feat(fundbox): add collaborators to fundbox detailed response

--- src/main/java/com/finapp/backend/dto/fundbox/FundBoxDetailsResponse.java
+++ src/main/java/com/finapp/backend/dto/fundbox/FundBoxDetailsResponse.java
@@ -18,15 +18,17 @@ public class FundBoxDetailsResponse {
     private LocalDate targetDate;
     private BigDecimal balance;
     private OwnerResponse owner;
+    private List<CollaboratorResponse> collaborators;
     private List<DepositResponse> deposits;
 
-    public FundBoxDetailsResponse(Long id, String name, BigDecimal financialGoal, LocalDate targetDate, OwnerResponse ownerResponse, BigDecimal balance, Page<DepositResponse> depositResponses) {
+    public FundBoxDetailsResponse(Long id, String name, BigDecimal financialGoal, LocalDate targetDate, OwnerResponse ownerResponse, BigDecimal balance, Page<DepositResponse> depositResponses, List<CollaboratorResponse> collaborators) {
         this.id = id;
         this.name = name;
         this.financialGoal = financialGoal;
         this.targetDate = targetDate;
         this.balance = balance;
         this.owner = ownerResponse;
+        this.collaborators = collaborators;
         this.deposits = depositResponses.getContent();
     }
 }
