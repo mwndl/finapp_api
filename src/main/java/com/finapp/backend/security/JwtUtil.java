f38511cd36b011d0ba3995a111e2e16fa6983293
“Marcos” <marcos.wiendl@gmail.com>
2025-04-23 21:19:05 -0300

    refactor(jwt): make 'isTokenValido' method validate only JWT structure, without checking validity

--- src/main/java/com/finapp/backend/security/JwtUtil.java
+++ src/main/java/com/finapp/backend/security/JwtUtil.java
@@ -62,10 +62,8 @@ public class JwtUtil {
 
     public boolean isTokenValid(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
-        return username.equals(userDetails.getUsername())
-                && !isTokenExpired(token);
+        return username.equals(userDetails.getUsername());
     }
-
 
     public boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
