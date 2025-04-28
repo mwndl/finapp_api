3f2b11bf05d81036dc13c10a47d585a4e28910ea
“Marcos” <marcos.wiendl@gmail.com>
2025-04-27 23:31:09 -0300

    refactor(service): create UserUtilsService and move user status check logic

--- src/main/java/com/finapp/backend/domain/service/UserService.java
+++ src/main/java/com/finapp/backend/domain/service/UserService.java
@@ -1,11 +1,14 @@
 package com.finapp.backend.domain.service;
 
+import com.finapp.backend.domain.event.PasswordChangedEvent;
+import com.finapp.backend.domain.service.utils.UserUtilService;
 import com.finapp.backend.dto.user.UserResponse;
 import com.finapp.backend.exception.ApiErrorCode;
 import com.finapp.backend.exception.ApiException;
 import com.finapp.backend.domain.model.User;
 import com.finapp.backend.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -17,32 +20,33 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-    private final AuthService authService;
+    private final ApplicationEventPublisher eventPublisher;
+    private final UserUtilService userUtilService;
 
     public UserResponse getUserInfo(String email) {
-        User user = getUserByEmail(email);
-        ensureAccountIsActive(user);
+        User user = userUtilService.getUserByEmail(email);
+        userUtilService.checkUserStatus(user);
 
         return new UserResponse(user.getName(), user.getEmail());
     }
 
     public void updateUserData(String email, String newName) {
-        User user = getUserByEmail(email);
-        ensureAccountIsActive(user);
+        User user = userUtilService.getUserByEmail(email);
+        userUtilService.checkUserStatus(user);
 
         if (newName != null && !newName.trim().isEmpty())
             updateUserName(user, newName);
     }
 
     public void updateUserPassword(String email, String currentPassword, String newPassword) {
-        User user = getUserByEmail(email);
-        ensureAccountIsActive(user);
+        User user = userUtilService.getUserByEmail(email);
+        userUtilService.checkUserStatus(user);
         updateUserPassword(user, currentPassword, newPassword);
     }
 
     public void requestAccountDeletion(String email) {
-        User user = getUserByEmail(email);
-        ensureAccountIsActive(user);
+        User user = userUtilService.getUserByEmail(email);
+        userUtilService.checkUserStatus(user);
 
         user.setActive(false);
         user.setDeletionRequestedAt(LocalDateTime.now());
@@ -50,22 +54,6 @@ public class UserService {
     }
 
     // aux methods
-    private User getUserByEmail(String email) {
-        return userRepository.findByEmail(email)
-                .orElseThrow(() -> new ApiException(ApiErrorCode.USER_NOT_FOUND));
-    }
-
-    private User getActiveUserByEmail(String email) {
-        User user = getUserByEmail(email);
-        ensureAccountIsActive(user);
-        return user;
-    }
-
-    private void ensureAccountIsActive(User user) {
-        if (!user.getActive())
-            throw new ApiException(ApiErrorCode.ACCOUNT_DEACTIVATED);
-    }
-
     private void updateUserName(User user, String newName) {
         if (newName.equals(user.getName()))
             throw new ApiException(ApiErrorCode.SAME_NAME);
@@ -90,7 +78,6 @@ public class UserService {
         user.setPasswordHash(passwordEncoder.encode(newPassword));
         userRepository.save(user);
 
-        authService.revokeAllUserSessions(user);
+        eventPublisher.publishEvent(new PasswordChangedEvent(user));
     }
-
 }
